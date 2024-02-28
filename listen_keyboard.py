ccc41b713e9470061ba7690892b9bf25ffece7ee
LeCua <ultrav8800@gmail.com>
2024-02-28 15:50:53 +0700

    x

--- listen_keyboard.py
+++ listen_keyboard.py
@@ -3,7 +3,7 @@ from evdev import InputDevice
 # ls /dev
 # Replace with the actual device path from step 2
 print("Listening for events [1]...")
-dev = InputDevice("/dev/input/event2")
+dev = InputDevice("/dev/input/event0")
 print("Listening for events [2]...")
 # Print key information for each event 
 for event in dev.read_loop():
