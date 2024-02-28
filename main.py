00f04638d46343961f5b440f7c50f22d703a4dec
LeCua <ultrav8800@gmail.com>
2024-02-28 15:19:00 +0700

    x

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyusb
+import pyautogui
+# pip install pyautogui
+# pip install edev
+
+# Replace these placeholders with your actual implementation
+def connect_to_reader():
+    # Code for connecting and identifying the reader
+
+def read_card_data():
+    # Code for reading data from the connected reader
+
+def card_scanned():
+    # Replace with code checking for new card scans using the reader
+
+# Main loop:
+while True:
+    if card_scanned():
+        data = read_card_data()
+        # Add suffix "DEMO" to the data
+        data_with_suffix = data + " DEMO"
+        pyautogui.write(data_with_suffix)
