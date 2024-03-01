b499c6390b22d17e40908d32e710cbe0542aabe7
LeCua <ultrav8800@gmail.com>
2024-03-01 15:26:29 +0700

    s

--- send_keyboard.py
+++ send_keyboard.py
@@ -75,6 +75,7 @@ def send_text_with_enter(text):
        for char in text:
             if char.isupper():
                 write_report(chr(32)+NULL_CHAR+chr(ord(char.lower()) - 93)+NULL_CHAR*5)
+                write_report(NULL_CHAR * 8)  # Release keys
             else:                  
                 write_report(NULL_CHAR * 2 + chr(ord(char) - 93) + NULL_CHAR * 5)
                 write_report(NULL_CHAR * 8)  # Release keys
